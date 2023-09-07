da597ca93141617edece0a653648bf662d8b5e26
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-07 13:15:27 +0530

    commit

--- model/model.py
+++ model/model.py
@@ -1,109 +1,91 @@
-import datetime
-import math
-import os
-import warnings
-
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from keras.layers import LSTM, Dense
-from keras.models import Sequential, load_model
+import matplotlib.pyplot as plt
+import math
 from sklearn.preprocessing import MinMaxScaler
-from tqdm import tqdm
-
-warnings.filterwarnings("ignore")
+from keras.models import Sequential , load_model
+from keras.layers import Dense, LSTM
 
 class ECHO_ECHO:
     
-    def __init__(self,dataset,saved_model_path):
-        self.dataset = dataset
-        self.dataframe = pd.read_csv(dataset)
-        self.dataframe_info = self.dataframe.info()
-        self.dataframe['Timestamp'] = pd.to_datetime(self.dataframe['Timestamp'])
-        self.dataframe_head = self.dataframe.head()
-        self.dataframe['Date'] = self.dataframe['Timestamp'].dt.date
-        self.scaler = MinMaxScaler(feature_range=(0,1))
-        self.saved_model_path = saved_model_path
+    def __init__(self,dataset_path,model_path):
+        self.dataset_path = dataset_path
+        self.model_path = model_path
+        self.dataframe = pd.read_csv(self.dataset_path)
+        self.sc = MinMaxScaler(feature_range=(0,1))
         
-    def visualize(self,show_plot=False):
-        self.dataframe.plot(x='Timestamp',y='PM2.5',figsize=(10,4))
-        plt.xticks(rotation=45)
-        if show_plot==True:
-            plt.show()
         
-    def process_data(self,num=60):
+    def data_preprocessing(self):
+        self.dataframe["Timestamp"] = pd.to_datetime(self.dataframe["Timestamp"])
+        self.dataframe['Date'] = self.dataframe["Timestamp"].dt.date
         self.dataframe_date = pd.DataFrame(self.dataframe.groupby('Date')['PM2.5'].mean())
-        self.datasets = self.dataframe_date.values
-        self.training_data_len = math.ceil(len(self.datasets)*0.8)
-        self.scaled_data = self.scaler.fit_transform(self.datasets)
+        self.dataframe_year = self.dataframe.loc[:,['Year','PM2.5']]
+        self.datafrane_day = self.dataframe.loc[:,['Day','PM2.5']]
+        self.dataframe_hour = self.dataframe.loc[:,['Hour','PM2.5']]
         
-        train_data = self.scaled_data[0:self.training_data_len,:]
-        self.x_train = []
+    def visualize_for_exsisting(self,show_graph=False):
+        self.new_dataframe_date = pd.DataFrame(self.dataframe.groupby('Date')['PM2.5'].mean())
+        if show_graph==True:
+            self.new_dataframe_date.plot(figsize=(25,4))
+            
+    def prepare_training_data(self):
+        self.dataset = self.dataframe_date.values
+        self.training_data_len = math.ceil(len(self.dataset)*0.8)
+        self.scaled_data = self.sc.fit_transform(self.dataset)
+        self.training_data = self.scaled_data[0:self.training_data_len,:]
+        self.X_train = []
         self.y_train = []
-        for i in range(num,len(train_data)):
-            self.x_train.append(train_data[i-num:i , 0])
-            self.y_train.append(train_data[i,0])
-        self.x_train, self.y_train = np.array(self.x_train), np.array(self.y_train)
-        self.x_train = np.reshape(self.x_train, (self.x_train.shape[0],self.x_train.shape[1],1))
+        for i in range(60,len(self.training_data)):
+            self.X_train.append(self.training_data[i-60:i,0])
+            self.y_train.append(self.training_data[i,0])
+        self.X_train, self.y_train = np.array(self.X_train), np.array(self.y_train)
+        self.X_train = np.reshape(self.X_train,(self.X_train.shape[0],self.X_train.shape[1],1))
         
-        # return self.x_train,self.y_train
-    
+            
     def build_model(self):
         self.model = Sequential()
-        self.model.add(LSTM(50, return_sequences=True, input_shape=(self.x_train.shape[1],1)))
-        self.model.add(LSTM(50,return_sequences=False))
-        self.model.add(Dense(25))
-        self.model.add(Dense(5))
-    
+        self.model.add(LSTM(units=50, return_sequences=True, input_shape=(self.X_train.shape[1],1)))
+        self.model.add(LSTM(units=50, return_sequences=False))
+        self.model.add(Dense(units=25))
+        self.model.add(Dense(units=1))
+        
+    def train_model(self):
+        self.model.compile(optimizer='adam',loss='mean_squared_error')
+        self.model.fit(self.X_train,self.y_train,batch_size=1,epochs=1)
+        self.model.save(self.model_path)
     
     def train_or_load_model(self):
-        if os.path.isfile(self.saved_model_path):
-            self.model = load_model(self.saved_model_path)
-        else:
-            self.model.compile(optimizer='adam',loss='mean_squared_error')
-            self.model.fit(self.x_train,self.y_train,epochs=25,batch_size=8)
-            self.model.save(self.saved_model_path)
-        
-    def make_prediction(self,num=60):
-        test_data = self.scaled_data[self.training_data_len-60:,:]
-        self.x_test = []
-        self.y_test = self.datasets[self.training_data_len:,:]
-        for i in range(num,len(test_data)):
-            self.x_test.append(test_data[i-num:i,0])
-        self.x_test = np.array(self.x_test)
-        self.x_test = np.reshape(self.x_test, (self.x_test.shape[0], self.x_test.shape[1], 1))
-        predictions = self.model.predict(self.x_test)
-        predictions = self.scaler.inverse_transform(predictions)
-        
-        return predictions
-    
-    def show_predicted_graph(self,prediction):
-        # prediction = prediction.reshape(-1,1)
-        rmse = np.sqrt(np.mean((prediction-self.y_test)**2))
-        train = self.dataframe_date[:self.training_data_len]
-        validate = self.dataframe_date[self.training_data_len:]
-        validate['Predictions'] = prediction
-        
-        plt.title("Model for PM2.5")
-        plt.xlabel("Date", fontsize = 15)
-        plt.ylabel("PM2.5",fontsize=15)
-        plt.plot(train[self.dataframe_date.columns])
-        plt.plot(validate['PM2.5'])
-        plt.plot(validate["Predictions"])
-        plt.legend(["Train","Val","Predictions"])
+        try:
+            self.model = load_model(self.model_path)
+        except:
+            self.build_model()
+            self.train_model()
             
+    def prepare_testing_data(self):
+        self.testing_data = self.scaled_data[self.training_data_len-60:,:]
+        self.X_test = []
+        self.y_test = self.dataset[self.training_data_len:,:]
+        for i in range(60,len(self.testing_data)):
+            self.X_test.append(self.testing_data[i-60:i,0])
+        self.X_test = np.array(self.X_test)
+        self.X_test = np.reshape(self.X_test,(self.X_test.shape[0],self.X_test.shape[1],1))
         
+    def make_prediction(self):
+        self.predictions = self.model.predict(self.X_test)
+        self.predictions = self.sc.inverse_transform(self.predictions)
         
-if __name__=='__main__':
     
-    dataset = 'model/dataset/air-quality-india.csv'
-    saved_model = 'model/saved_model/echo_echo.h5'
-    
-    echo_echo = ECHO_ECHO(dataset,saved_model)
-    echo_echo.visualize(show_plot=False)
-    echo_echo.process_data()
-    echo_echo.build_model()
-    echo_echo.train_or_load_model()
-    prediction = echo_echo.make_prediction(num=60)
-    # print(prediction)
-    echo_echo.show_predicted_graph(prediction=prediction)
+    def visualize_prediction(self,save_graph=False):
+        self.train = self.new_dataframe_date[:self.training_data_len]
+        self.valid = self.new_dataframe_date[self.training_data_len:]
+        self.valid['Predictions'] = self.predictions
+        plt.figure(figsize=(10,4))
+        plt.title('Model')
+        plt.xlabel('Date',fontsize=8)
+        plt.ylabel('PM2.5',fontsize=8)
+        plt.plot(self.train['PM2.5'])
+        plt.plot(self.valid[['PM2.5','Predictions']])
+        plt.legend(['Train','Val','Predictions'])
+        plt.show()
+        if save_graph==True:
+            plt.savefig('./graph/prediction.png')
---
+++ test_run.py
@@ -0,0 +1,16 @@
+from model.model import ECHO_ECHO
+
+echo_echo = ECHO_ECHO(
+    dataset_path='dataset/air-quality-india.csv',
+    model_path='saved_model/echo_echo.h5'
+)
+
+if __name__ == "__main__":
+    
+    echo_echo.data_preprocessing()
+    echo_echo.visualize_for_exsisting(show_graph=False)
+    echo_echo.prepare_training_data()
+    echo_echo.train_or_load_model()
+    echo_echo.prepare_testing_data()
+    echo_echo.make_prediction()
+    echo_echo.visualize_prediction()
