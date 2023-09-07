0f83cd09b452e20abe79fb2046fdb6692c7c50f6
Vivek Thakur <20509.vivek@gpranchi.org>
2023-09-08 00:36:05 +0530

    commit

--- test_run.py
+++ test_run.py
@@ -1,16 +1,16 @@
-# from model.model import AeroMax
+from model.model import AeroMax
 
-# aeromax = AeroMax(
-#     dataset_path='dataset/air-quality-india.csv',
-#     model_path='saved_model/aeromax_predictor_model.h5'
-# )
+aeromax = AeroMax(
+    dataset_path='dataset/air-quality-india.csv',
+    model_path='saved_model/aeromax_predictor_model.h5'
+)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     aeromax.data_preprocessing()
-#     aeromax.visualize_for_exsisting(show_graph=False)
-#     aeromax.prepare_training_data()
-#     aeromax.train_or_load_model()
-#     aeromax.prepare_testing_data()
-#     aeromax.make_prediction()
-#     aeromax.visualize_prediction()
+    aeromax.data_preprocessing()
+    aeromax.visualize_for_exsisting(show_graph=False)
+    aeromax.prepare_training_data()
+    aeromax.train_or_load_model()
+    aeromax.prepare_testing_data()
+    aeromax.make_prediction()
+    aeromax.visualize_prediction()
